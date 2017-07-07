73273062d206c46bc501c6db6a9c612f32972148
unknown <manish.bhavnani@BVAHDWS006.brainvire.com>
2017-07-07 17:41:50 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,26 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { ContentDrawer } from '../pages/contentdrawer/contentdrawer';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+     ContentDrawer
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
+    IonicModule.forRoot(MyApp,{
+    menuType: 'push',
+    platforms: {
+      ios: {
+        menuType: 'overlay',
+      }
+    }
+  })],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { MenuController } from 'ionic-angular';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
 
-  }
+ drawerOptions: any;
+ 
+    constructor(public navCtrl: NavController,public menuCtrl: MenuController) {}
+
+   openMenu() {
+   this.menuCtrl.open();
+ }
+
+ 	closeMenu() {
+   this.menuCtrl.close();
+ }
+
+ 	toggleMenu() {
+   this.menuCtrl.toggle();
+ }
 
 }
